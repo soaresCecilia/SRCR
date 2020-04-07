5d7067eccfb15bcea4e946f715a9f95602832b0a
soaresCecilia <cecilia.oliveira.soares@gmail.com>
2020-04-07 18:20:16 +0100

    Funções iniciais

---
+++ funcoesAuxiliares.pl
@@ -0,0 +1,86 @@
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Funções Auxiliares
+
+% Encontra todas as soluções
+solucoes(X, XS, R) :- XS, assert(tmp(X)), fail.
+solucoes(X, XS, R) :- solucoesAux([], R).
+
+solucoesAux(L, R) :- retract(tmp(X)), !, solucoesAux([X|L], R).
+solucoesAux(R, R).
+
+% Comprimento de uma lista
+comprimento([], 0).
+comprimento([_|XS], R) :- comprimento(XS, S), R is 1 + S.
+
+% Inserção de conhecimento
+insercao( Termo ) :-
+assert( Termo ).
+insercao( Termo ) :-
+retract( Termo ),!,fail.
+
+% Remoção de conhecimento
+remocao( Termo ) :-
+retract( Termo ).
+remocao( Termo ) :-
+assert( Termo ),!,fail.
+
+% Testa se todos os predicados são verdadeiros
+teste( [] ).
+teste( [R|LR] ) :-
+R,
+teste( LR ).
+
+% Insere novo conhecimento na base de conhecimento
+evolucao( Termo ) :-
+solucoes( Invariante,+Termo::Invariante,Lista ),
+insercao( Termo ),
+teste( Lista ).
+
+% Retira conhecimento da base de conhecimento
+involucao( Termo ) :-
+solucoes( Invariante,-Termo::Invariante,Lista ),
+remocao( Termo ),
+teste( Lista ).
+
+% Verifica se o elemento A pertence a uma lista
+pertence( X,[X|L] ).
+pertence( X,[Y|L] ) :- X \= Y, pertence( X,L ).
+
+
+% Encontra todas as solucoes e coloca-as na lista L.
+solucoes( X,Y,Z ) :- findall( X,Y,Z ).
+
+
+% Verifica o comprimento.
+comprimento( S,N ) :- length( S,N ).
+
+
+% Extensao do meta-predicado demo: Questao,Resposta -> {V,F}
+%                            Resposta = verdadeiro,falso,desconhecido }
+
+demo( Questao,verdadeiro ) :-
+Questao.
+demo( Questao,falso ) :-
+-Questao.
+demo( Questao,desconhecido ) :-
+nao( Questao ),
+nao( -Questao ).
+
+
+% Extensao do meta-predicado nao: Questao -> {V,F}
+
+nao( Questao ) :-
+Questao, !, fail.
+nao( Questao ).
+
+
+% Nif válido (com 9 digitos)
+nifValido(Nif) :- comprimento(Nif, R), R = 9.
+
+% Ganho ou custo válido (>= 0)
+custoValido(G) :- G >= 0.
+
+% Tipo de Procedimento ou é Ajuste Direto, Consulta Prévia ou Concurso Público.
+tipoProcedimentoValido("Ajuste Direto").
+tipoProcedimentoValido("Consulta Prévia").
+tipoProcedimentoValido("Concurso Público").
---
+++ trabalho1.pl
@@ -0,0 +1,110 @@
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SIST. REPR. CONHECIMENTO E RACIOCINIO - MiEI/3
+
+% Sistema de Representação de conhecimento e raciocínio com capacidade
+% para caracterizar um universo de discurso na área da prestação de
+% cuidados de saúde
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: Declaracoes iniciais
+
+:- set_prolog_flag( discontiguous_warnings,off ).
+:- set_prolog_flag( single_var_warnings,off ).
+:- set_prolog_flag( unknown,fail ).
+
+:- op(900,xfy,'::').
+:- dynamic adjudicante/4.
+:- dynamic adjudicatario/4.
+:- dynamic contrato/9.
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Carregar predicados do ficheiro no qual é guardado o estado
+
+:- include('funcoesAuxiliares.pl').
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Invariantes Estruturais e Referenciais
+
+%--------- Adjudicatarios
+% Garantir que o id de cada adjudicatario é único
++adjudicatario(Id,Nome, Nif, Morada) :: (solucoes(Id, adjudicatario(Id,_,_,_), L),
+comprimento(L, 1)).
+
+% Garantir que adjudicatarios com ids diferentes têm diferente informação
++adjudicatario(Id,Nome,Nif, Morada) :: (solucoes((Nome, Nif, Morada), adjudicatario(_,Nome, Nif, Morada), L),
+comprimento(L, 1)).
+
+%Garantir que o Nif do adjudicatário é válido.
++adjudicatario(_,_, Nif, _) :: nifValido(Nif).
+
+
+% Garantir que não é possível remover um adjudicatario que celebrou contratos públicos.
+-adjudicatario(Id,_,_,_,_) :: (solucoes(Id, contrato(_,Id,_,_,_,_,_,_,_), L),
+comprimento(L, 0)).
+
+
+%--------- Adjudicantes
+% Garantir que o id de cada adjudicante é único
++adjudicante(Id,Nome, Nif, Morada) :: (solucoes(Id, adjudicante(Id,_,_,_), L),
+comprimento(L, 1)).
+
+% Garantir que adjudicatarios com ids diferentes têm diferente informação
++adjudicante(Id,Nome,Nif, Morada) :: (solucoes((Nome, Nif, Morada), adjudicante(_,Nome, Nif, Morada), L),
+comprimento(L, 1)).
+
+%Garantir que o Nif do adjudicatário é válido.
++adjudicante(_,_, Nif, _) :: nifValido(Nif).
+
+
+% Garantir que não é possível remover um adjudicante que celebrou contratos públicos.
+-adjudicante(Id,_,_,_) :: (solucoes(Id, contrato(_,Id,_,_,_,_,_,_,_), L),
+comprimento(L, 0)).
+
+
+%--------- Contratos
+
+% Garantir que cada contrato é único
++contrato(IdAd, IdAda, TipoContrato, TipoProcedimento, Descricao, Valor, Prazo, Local, Data) ::
+(solucaoes((IdAd, IdAda, TipoContrato, TipoProcedimento, Descricao, Valor, Prazo, Local, Data),
+contrato(IdAd, IdAda, TipoContrato, TipoProcedimento, Descricao, Valor, Prazo, Local, Data), L),
+comprimento(L, 1)).
+
+% Garantir que não é possível remover um contrato associado a um adjudicante
+-contrato(IdAdjudicante,_,_,_,_,_,_,_,_) :: (solucoes(IdAdjudicante, adjudicante(IdAdjudicante,_,_,_), L),
+comprimento(L, 0)).
+
+% Garantir que não é possível remover um contrato associado a um adjudicatário
+-contrato(_,IdAdjudicatario,_,_,_,_,_,_,_) :: (solucoes(IdAdjudicatario, adjudicatario(IdAdjudicatario,_,_,_), L),
+comprimento(L, 0)).
+
+% Garantir que o tipo de procedimento é válido.
++contrato(_,_,_,TP,_,_,_,_,_) :: tipoProcedimentoValido(TP).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Adicionar adjudicatarios, adjudicantes e contratos.
+
+novoAdjudicatario(Id,Nif,Nome, Morada) :- evolucao(adjudicatario(Id,Nif,Nome, Morada)).
+
+novoAdjudicante(Id,Nif,Nome, Morada) :- evolucao(adjudicante(Id,Nif,Nome, Morada)).
+
+novoContrato(IdAd,IdAda,TC,TP,Desc,Valor,Prazo,Local,Data) :- evolucao(contrato(IdAd,IdAda,TC,TP,Desc,Valor,Prazo,Local,Data)).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Remover adjudicatarios, adjudicantes e contratos.
+
+%Devolve Adjudicante por Id do mesmo.
+adjudicanteID(Id, L) :- solucoes(adjudicante(Id,Nif, Nome, Morada), adjudicante(Id,Nif, Nome, Morada), L).
+
+removeAdjudicante(Id) :- adjudicanteID(Id,[X|_]), involucao(X).
+
+
+
+adjudicatarioID(Id, L) :- solucoes(adjudicatario(Id,Nif, Nome, Morada), adjudicatario(Id,Nif, Nome, Morada), L).
+
+removeAdjudicatario(Id) :- adjudicatarioID(Id,[X|_]), involucao(X).
+
+
